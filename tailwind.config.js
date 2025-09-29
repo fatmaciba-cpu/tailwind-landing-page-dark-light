3d9b5c88d92b4c47c3201c8a55a86db6d596ef36
Fatma <fatma.ciba@voltrun.com.tr>
2025-09-29 16:35:12 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // veya 'media'
+  content: [
+    "./src/**/*.{html,js}", // proje dosyalarını buraya ekle
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
